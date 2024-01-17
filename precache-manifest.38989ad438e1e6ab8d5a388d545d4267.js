a72c3c90eb4e9989453e53968e4074c0680b50de
Sarah Wang <sarah.wang2009@gmail.com>
2024-01-17 10:46:19 -0500

    Updates

---
+++ precache-manifest.38989ad438e1e6ab8d5a388d545d4267.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaf5bd80042a8dcb6a36ca9987f18df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1adc87186ee031ac1fd",
+    "url": "/static/css/main.fafe1f06.chunk.css"
+  },
+  {
+    "revision": "1eff2b6c3a9d24884eb2",
+    "url": "/static/js/2.2f8c7507.chunk.js"
+  },
+  {
+    "revision": "a1adc87186ee031ac1fd",
+    "url": "/static/js/main.2df95afb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2fd8b270ef9bae9465b8beaecb1e5473",
+    "url": "/static/media/github.2fd8b270.svg"
+  },
+  {
+    "revision": "915341afeb0aa504d2c236a781e583b5",
+    "url": "/static/media/gmail.915341af.svg"
+  },
+  {
+    "revision": "a70356a7321f4caa66739552e3debaae",
+    "url": "/static/media/linkedin.a70356a7.svg"
+  },
+  {
+    "revision": "4cb0a35789120d800b5ead60b3ae501e",
+    "url": "/static/media/profile.4cb0a357.JPG"
+  }
+]);
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import { About } from "./About";
-import { Home } from "./Home";
-import { Work } from "./Work";
-
-export { About, Home, Work };
